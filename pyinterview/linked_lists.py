32860e4f9ccf126404ef5b8d110187c607e0bd0e
Marwan Hawari <marwan.hawari@gmail.com>
2021-09-02 17:52:45 -0700

    Added linked list docstrings

--- pyinterview/linked_lists.py
+++ pyinterview/linked_lists.py
@@ -1,18 +1,45 @@
+from typing import Union
+
+
 class ListNode:
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
 
-def LL_to_list(head: ListNode) -> list:
+def LL_to_array(head: ListNode) -> list:
+    """Convert a linked list into an array.
+
+    Args:
+        head (ListNode): The head node of the linked list.
+
+    Returns:
+        list: A new array containing the values from the linked list in order.
+    """
     return []
 
 
-def list_to_LL(array: list) -> ListNode:
+def array_to_LL(array: list) -> ListNode:
+    """Convert an array into a linked list.
+
+    Args:
+        array (list): The array which will be converted.
+
+    Returns:
+        ListNode: The head node of the new linked list.
+    """
     return ListNode(0)
 
 
 def reverse_LL(head: ListNode) -> ListNode:
+    """Reverse a linked list in place.
+
+    Args:
+        head (ListNode): The head node of the linked list.
+
+    Returns:
+        ListNode: The head node of the reversed linked list.
+    """
     itr = head
     prev = None
 
@@ -26,12 +53,40 @@ def reverse_LL(head: ListNode) -> ListNode:
 
 
 def pop_LL(head: ListNode) -> ListNode:
+    """Pop the last element from a linked list.
+
+    Args:
+        head (ListNode): The head node of the linked list.
+
+    Returns:
+        ListNode: The element that used to be the last element of the linked list.
+    """
     return ListNode(0)
 
 
-def append_LL(head: ListNode) -> ListNode:
+def append_LL(head: ListNode, element: Union[int, float, str]) -> ListNode:
+    """Append an element to the end of a linked list.
+
+    Args:
+        head (ListNode): The head node of the linked list.
+        element (Union[int, float, str]): The value of the ListNode element
+        to be appended.
+
+    Returns:
+        ListNode: The head node of the linked list.
+    """
     return ListNode(0)
 
 
-def remove_LL(head: ListNode) -> ListNode:
+def remove_LL(head: ListNode, element: Union[int, float, str]) -> ListNode:
+    """Removes the first node in the linked list that has a given value.
+
+    Args:
+        head (ListNode): The head node of the linked list.
+        element (Union[int, float, str]): The value of the ListNode element
+        to be removed.
+
+    Returns:
+        ListNode: The head node of the linked list.
+    """
     return ListNode(0)
