a7d232898b3e0e66e19e36dad0ba0edbe3eee758
Marwan Hawari <marwan.hawari@gmail.com>
2021-09-05 10:00:40 -0700

    added graph tests for all functions

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,5 +1,11 @@
 import pytest
-from pyinterview.graphs import undirected_adj_list
+from collections import deque
+from pyinterview.graphs import (
+    undirected_adj_list,
+    directed_adj_list,
+    inbound_degrees,
+    find_sources,
+)
 
 
 @pytest.mark.parametrize(
@@ -11,3 +17,38 @@ from pyinterview.graphs import undirected_adj_list
 )
 def test_undirected_adj_list(edges, expected):
     assert undirected_adj_list(edges) == expected
+
+
+@pytest.mark.parametrize(
+    ("edges", "expected"),
+    [
+        ([("A", "B"), ("A", "C")], {"A": ["B", "C"], "B": [], "C": []}),
+        ([("A", "B"), ("B", "C")], {"A": ["B"], "B": ["C"], "C": []}),
+    ],
+)
+def test_directed_adj_list(edges, expected):
+    assert directed_adj_list(edges) == expected
+
+
+@pytest.mark.parametrize(
+    ("adj_list", "expected"),
+    [
+        ({"A": ["B", "C"], "B": [], "C": []}, {"A": 0, "B": 1, "C": 1}),
+        ({"A": ["B"], "B": ["C"], "C": []}, {"A": 0, "B": 1, "C": 1}),
+        ({"A": ["B"], "B": ["A"]}, {"A": 1, "B": 1}),
+    ],
+)
+def test_inbound_degrees(adj_list, expected):
+    assert inbound_degrees(adj_list) == expected
+
+
+@pytest.mark.parametrize(
+    ("inbounnd_degrees", "expected"),
+    [
+        ({"A": 0, "B": 1, "C": 1}, deque(["A"])),
+        ({"A": 0, "B": 1, "C": 1}, deque(["A"])),
+        ({"A": 1, "B": 1}, deque([])),
+    ],
+)
+def test_find_sources(inbounnd_degrees, expected):
+    assert find_sources(inbounnd_degrees) == expected
